a27fda6b2438de0a063a52c7cdc50a159dbcf1eb
smolsavibigcity <fakeblue10901@gmail.com>
2026-02-08 14:20:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-// --- LEVEL 1: PUNE SEAFOOD MAP ---
-// 1. Center the map near Bund Garden
-const map = L.map('map').setView([18.5401, 73.8834], 15);
+// --- LEVEL 1: SIMPLIFIED PUNE MAP ---
+// 1. Center map and set a much higher zoom level (16 instead of 15)
+const map = L.map('map').setView([18.539, 73.881], 16); 
+
+// Sleek dark tiles
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(map);
+
+// 2. Add a BIG "Goal Zone" so he knows where to go
+const targetCircle = L.circle([18.53755, 73.87965], {
+    color: 'gold',
+    fillColor: '#ffd700',
+    fillOpacity: 0.3,
+    radius: 80 // Big visible circle
+}).addTo(map).bindPopup("<b>SOUP DESTINATION</b>").openPopup();
 
 const usIcon = L.divIcon({
     className: 'couple-marker',
-    html: '<img src="us.jpg" style="width:50px;height:50px;border-radius:50%">',
-    iconSize: [50, 50]
+    html: '<img src="us.jpg" style="width:60px;height:60px;border-radius:50%; border: 3px solid gold;">',
+    iconSize: [60, 60]
 });
 
-// 2. The Destination: Suonmoi (Latitude, Longitude)
-const soupLocation = L.latLng(18.53755, 73.87965); 
-
-// 3. The Start: Sujata Apartment (Latitude, Longitude)
+// 3. The Start: Sujata Apartment
 const marker = L.marker([18.54014, 73.88345], {icon: usIcon, draggable: true}).addTo(map);
 
+// 4. Easy-mode check
 marker.on('dragend', function() {
-    // If he gets us within ~150 meters of the restaurant
-    if (marker.getLatLng().distanceTo(soupLocation) < 150) {
-        nextLevel(2); // This unlocks the Chess level!
+    const distance = marker.getLatLng().distanceTo(targetCircle.getLatLng());
+    
+    // If he gets within 150 meters (very generous!), he wins
+    if (distance < 150) {
+        nextLevel(2);
     }
 });
